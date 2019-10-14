e8403e2016e4ad7a599a1f3313519532f48ba05a
Rui Alves <up201606746@fe.up.pt>
2019-10-14 15:26:46 +0100

    Added cve list page scraping to build cve list

--- src/CVEListFetcher.js
+++ src/CVEListFetcher.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 const getCVEListPageUrl = (search_query) => (
     `https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=${encodeURI(search_query)}`
@@ -9,7 +10,21 @@ const fetchCVEListPage = (search_query) => {
     return axios.get(page_url);
 };
 
+const parseRawCVE = (raw_cve) => raw_cve.substring(4);
+
+const getCVEList = async (search_query) => {
+    const res = await fetchCVEListPage(search_query);
+    const $ = cheerio.load(res.data);
+    const cve_entries = $("#TableWithRules table tbody tr td:first-child a");
+
+    return cve_entries.toArray().map((entry) => (
+        parseRawCVE(entry.children[0].data)
+    ));
+};
+
 module.exports = {
     getCVEListPageUrl,
     fetchCVEListPage,
+    getCVEList,
+    parseRawCVE,
 };
