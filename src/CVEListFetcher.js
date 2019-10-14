bed5b3349afaff012b99c755bf4eca26654933fc
Rui Alves <up201606746@fe.up.pt>
2019-10-14 15:20:38 +0100

    Added CVE list page fetching

--- src/CVEListFetcher.js
+++ src/CVEListFetcher.js
@@ -1,7 +1,15 @@
+const axios = require("axios");
+
 const getCVEListPageUrl = (search_query) => (
     `https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=${encodeURI(search_query)}`
 );
 
+const fetchCVEListPage = (search_query) => {
+    const page_url = getCVEListPageUrl(search_query);
+    return axios.get(page_url);
+};
+
 module.exports = {
     getCVEListPageUrl,
+    fetchCVEListPage,
 };
