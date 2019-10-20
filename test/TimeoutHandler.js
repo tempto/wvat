f7cd26803ab3a1c81199b082f7beeb5d1ce79a3e
Tiago Ribeiro <tiago7b27@gmail.com>
2019-10-20 22:09:16 +0100

    Tried to test function timeout without success

---
+++ test/TimeoutHandler.js
@@ -0,0 +1,19 @@
+const TimeoutHandler = require("../src/TimeoutHandler.js");
+const {expect, test} = require('@oclif/test')
+
+
+
+describe('TimeoutHandler tests', () => {
+
+    /*
+    test
+    .stdout()
+    .command(['hello','-t','1'])
+    .catch(err => expect(err.message).equal('EEXIT: 1'))
+    .exit(1)
+    .it('Test if program timeout')
+    */
+
+    
+
+});
