b66210c7690a5996b06d692a24c6c76d8e69b990
Rui Alves <up201606746@fe.up.pt>
2019-10-21 10:51:40 +0100

    Added cves fetching command

---
+++ src/commands/cves.js
@@ -0,0 +1,26 @@
+const { Command } = require("@oclif/command");
+const { getCVEList } = require("../CVEListFetcher");
+
+class CVECommand extends Command {
+    async run() {
+        const { args } = this.parse(CVECommand);
+        const technology = args.technology;
+
+        const cve_list = await getCVEList(technology);
+
+        this.log(cve_list);
+    }
+}
+
+CVECommand.description = "Searches CVEs for a given technology";
+
+CVECommand.args = [
+    {
+        name: "technology",
+        required: true,
+        description: "technology to search CVEs",
+        hidden: false,
+    },
+];
+
+module.exports = CVECommand;
