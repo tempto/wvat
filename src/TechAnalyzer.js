9fcb4cd9cbc15769279953ee7ebeac510359c103
Rekicho <up201604145@fe.up.pt>
2019-11-04 10:41:49 +0000

    feat: Added function to calculate no version tech
    Counts elements of technologies arrays that have a null version attribute
    Also added javadoc for TechAnalyzer

--- src/TechAnalyzer.js
+++ src/TechAnalyzer.js
@@ -1,6 +1,11 @@
 const Wappalyzer = require("wappalyzer");
 const { isValidURL } = require("../src/utils");
 
+/**
+ * Analyses webpage technologies using Wappalyzer
+ * @param {string} url Webpage url
+ * @returns {Array} Wappalyzer analysis results
+ */
 const analyseWebPage = async (url) => {
     const wapp = new Wappalyzer(url);
     const res = await wapp.analyze();
@@ -8,12 +13,25 @@ const analyseWebPage = async (url) => {
     return res;
 };
 
+/**
+ * Parses webpage url technologies analysis results
+ * @param {string} url Webpage url
+ * @param {Array} tech Technology array from analyseWebPage array following this format
+ * @returns {Array} Array with found technologies
+ */
 const parseAnalysisResults = (url, tech) => {
     if (tech.urls[url].status !== 200) throw new Error("Could not access webpage");
 
     return tech.applications;
 };
 
+/**
+ * Obtains webpage technologies
+ * @param {string} url Webpage url
+ * @throws {Error} Missing webpage url
+ * @throws {Error} Invalid url
+ * @returns {Array} Webpage technologies
+ */
 const getWebpageTechnologies = async (url) => {
     if (!url) throw new Error("Missing Webpage url");
     if (!isValidURL(url)) throw new Error("Invalid url");
@@ -24,7 +42,19 @@ const getWebpageTechnologies = async (url) => {
     return parseAnalysisResults(fixed_url, tech);
 };
 
+/**
+ * Counts technologies with no version
+ * @param {Array} tech Array with technologies
+ * @throws {Error} Missing technologies
+ * @returns {Number} Technologies with no version count
+ */
+const noVersionCount = (tech) => {
+    if (!tech) throw new Error("Missing technologies");
+    return tech.reduce((count, currElem) => count + (currElem.version === null ? 1 : 0), 0);
+};
+
 module.exports = {
     getWebpageTechnologies,
     parseAnalysisResults,
+    noVersionCount,
 };
