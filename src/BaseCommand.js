2db1717a2b911b782a65eb475552804a465e4ee6
Rui Alves <up201606746@fe.up.pt>
2019-11-09 18:31:50 +0000

    enhancement: Added documentation to the BaseCommand class
    Improved the class documentation

--- src/BaseCommand.js
+++ src/BaseCommand.js
@@ -1,7 +1,15 @@
 const { Command } = require("@oclif/command");
 const Sentry = require("@sentry/node");
 
+/**
+ * Base command that is extended by all the 'callable' tool's commands
+ */
 class BaseCommand extends Command {
+    /**
+     * Caputers all errors that occur in the commands execution, capturing them in Sentry (Error Middleware)
+     * @param {Error} err Thrown error
+     * @throws {Error} Captured error
+     */
     catch(err) {
         const sentry_issue_id = Sentry.captureException(err);
         this.log(err);
