f100edc5a958c28f6a11ad0b2358e487cf3a2e66
Rui Alves <up201606746@fe.up.pt>
2019-11-09 18:20:52 +0000

    enhancement: Added documentation to HandleTimeout module
    Improved the module's functions documentation

--- src/handleTimeout.js
+++ src/handleTimeout.js
@@ -1,3 +1,8 @@
+/**
+ * Sets up a timeout for the application
+ * @param {function} callback Function to call after timeout
+ * @param {number} time Time (in seconds) until timeout
+ */
 const handleTimeout = (callback, time) => {
     setTimeout(callback, time * 1000);
 };
