cf2cdda236cc8347e75f886b06a700185d21ebcd
Angelo Teixeira <imnotteixeira@gmail.com>
2019-11-19 13:24:10 +0000

    feat: Added subdomain crawling via amass tool

--- src/SubdomainCrawler.js
+++ src/SubdomainCrawler.js
@@ -1,20 +1,65 @@
 const commandExists = require("command-exists");
+const fs = require("fs");
+const exec = require("child_process").exec;
+
+const isValidURL = require("./utils").isValidURL;
 const Logger = require("./Logger");
 const Config = require("./Config");
 
 const Log = new Logger(Config.flags).getLog();
 
-const checkGoExists = () => {
+const checkGo = () => {
     commandExists("go")
         .catch(() => {
-            Log.error("This tool requires Go, but wasn't found on this machine!");
-            process.exit(0);
+            throw new Error("Golang not found. It is required for this tool to work");
         });
 };
 
-const getSubdomainsList = () => {
-    checkGoExists();
-};
+const DEFAULT_TIMEOUT_MINS = 1;
+
+/**
+ * @typedef {Object} OptionsObject
+ * @property {number} timeout - Max crawling time, in minutes
+ * @param {String} domain url for the domain to lookup
+ * @param {OptionsObject} options [optional] options object
+ */
+const getSubdomainsList = (domain, options) => new Promise((resolve, reject) => {
+
+    if (!isValidURL(domain)) {
+        reject(new Error("Invalid domain format, please use a valid URL."));
+    }
+
+    let cmd = `./amass enum -d ${domain} -o amass-output -json amass-output.json `;
+
+    let timeout;
+    if (!options || !options.timeout) timeout = DEFAULT_TIMEOUT_MINS;
+    else timeout = options.timeout;
+
+    cmd += `-timeout ${timeout} `;
+    const amass = exec(cmd);
+
+    amass.stdout.on("data", (data) => {
+        Log.info(`AMASS: ${data}`);
+    });
+    amass.stderr.on("data", (data) => {
+        Log.error(`AMASS: ${data}`);
+    });
+
+    amass.on("close", () => {
+        const fileDataTxt = fs.readFileSync("../bin/amass-output", "utf-8");
+        // Using splice because the last element is an empty string
+        const subdomains = fileDataTxt.split("\n").slice(0, -1);
+
+        Log.info(`Found the following subdomains for domain ${domain}:`);
+        Log.info(subdomains);
+
+        resolve(subdomains);
+
+        // console.log("Extra-info");
+        // const fileData = fs.readFileSync("amass-output.json", "utf-8");
+        // console.log(fileData.split("\n").slice(0, -1).map(JSON.parse));
+    });
+});
 
 module.exports = {
     getSubdomainsList,
--- src/commands/crawl.js
+++ src/commands/crawl.js
@@ -7,15 +7,24 @@ class CrawlerCommand extends Command {
     async run() {
         const { args, flags } = this.parse(CrawlerCommand);
 
-        const domain = args.domain;
         const { depth, noCrawlingCache } = flags;
-        const { getSubdomainsList } = require("../SubdomainCrawler");
-        const { getPagesList } = require("../PageCrawler");
-        getSubdomainsList();
-        const domain_list = await getPagesList(domain, depth, noCrawlingCache);
+        const { domain } = args;
 
-        if (domain_list.length) {
-            Logger.print(JSON.stringify(domain_list, null, 2));
+        const { getSubdomainsList } = require("../SubdomainCrawler");
+        const subdomains_list = await getSubdomainsList(domain);
+
+        const { getPagesList } = require("../PageCrawler");
+
+        const crawl_tree = {};
+
+        await Promise.all(subdomains_list.map(async (subdomain, i) => {
+            Logger.info(`${i}Fetching pages for subdomain ${subdomain}`);
+            const pages_list = await getPagesList(`https://${domain}`, depth, noCrawlingCache);
+            crawl_tree[subdomain] = pages_list ? pages_list : [];
+        }));
+
+        if (subdomains_list.length) {
+            Logger.print(JSON.stringify(subdomains_list, null, 2));
         } else {
             Logger.error(Errors.NO_SUBDOMAINS_FOUND.description);
             process.exit(Errors.NO_SUBDOMAINS_FOUND.code);
