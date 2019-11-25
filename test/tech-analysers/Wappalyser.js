700e5091596878adb6b53d916d7f86e50cfb5d31
Rekicho <up201604145@fe.up.pt>
2019-11-25 19:32:23 +0000

    enhancement: Changed Wappalyser tests
    Changed Wappalyser based on previous Wappalyser changes

---
+++ test/tech-analysers/Wappalyser.js
@@ -0,0 +1,51 @@
+const Wappalyser = require("../../src/tech-analysers/Wappalyser");
+
+describe("Wappalyser tests", () => {
+    describe("Validate getWebpageTechnologies arguments", () => {
+        it("should fail when the url is missing", () => {
+            expect.assertions(1);
+            Wappalyser.getWebpageTechnologies().catch((e) => {
+                expect(e).toEqual(new Error("Missing Webpage url"));
+            });
+        });
+        it("should fail when the given url is not valid", () => {
+            expect.assertions(1);
+            Wappalyser.getWebpageTechnologies("hppts://www.gitlab.com/").catch((e) => {
+                expect(e).toEqual(new Error("Invalid url"));
+            });
+        });
+    });
+    describe("Parse results", () => {
+        it("should fail when the webpage response is not 200", () => {
+            const tech = {
+                urls: {
+                    "https://www.gitlab.com": {
+                        status: 404,
+                    },
+                },
+            };
+
+            expect(() => Wappalyser.parseAnalysisResults("https://www.gitlab.com", tech)).toThrow("Could not access webpage");
+        });
+        it("should properly parse the technologies", () => {
+            const tech = {
+                urls: {
+                    "https://www.gitlab.com": {
+                        status: 200,
+                    },
+                },
+                applications: [
+                    { name: "Varnish",
+                        confidence: "100",
+                        version: null,
+                        icon: "Varnish.svg",
+                        website: "http://www.varnish-cacher.org",
+                        categories: [],
+                    },
+                ],
+            };
+
+            expect(Wappalyser.parseAnalysisResults("https://www.gitlab.com", tech)).toEqual(tech.applications);
+        });
+    });
+});
