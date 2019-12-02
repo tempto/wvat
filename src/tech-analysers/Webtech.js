0ffaf7eec9997db25391d9505f6085d688c9b942
Tiago Ribeiro <tiago7b27@gmail.com>
2019-12-02 00:32:11 +0000

    feat: Added Webtech Tech-analyser

---
+++ src/tech-analysers/Webtech.js
@@ -0,0 +1,58 @@
+const TechAnalyser = require("../TechAnalyser");
+const spawn = require("await-spawn");
+const { isValidURL, addURLEndSlash } = require("../utils");
+
+class Wappalyser extends TechAnalyser {
+    /**
+     * Analyses webpage technologies using Webtech
+     * @param {string} url Webpage url
+     * @throws {Error} Could not access webpage
+     * @returns {Array} Webtech analysis results
+     */
+    static async analyseWebPage(url) {
+        const tech = await spawn("./bin/webtech/webtech-macOS", ["--urls", url]);
+        return tech.toString();
+    }
+
+    /**
+     * Parses webpage url technologies analysis results
+     * @param {string} url Webpage url
+     * @param {Array} tech Technology array from analyseWebPage array following this format
+     * @returns {Array} Array with found technologies
+     */
+    static parseAnalysisResults(url, tech) {
+        const str = tech.toString(), lines = str.split(/(\r?\n)/g);
+        const techs = [];
+        for (let i = 4; i < lines.length; i += 2) {
+            if (lines[i] !== "") {
+                techs.push(lines[i].slice(3, -1));
+            }
+        }
+
+        const version = null;
+
+        return techs.map((name) => ({
+            name,
+            version,
+        }));
+    }
+
+    /**
+     * Obtains webpage technologies
+     * @param {string} url Webpage url
+     * @throws {Error} Missing webpage url
+     * @throws {Error} Invalid url
+     * @returns {Array} Webpage technologies
+     */
+    static async getWebpageTechnologies(url) {
+        if (!url) throw new Error("Missing Webpage url");
+        if (!isValidURL(url)) throw new Error("Invalid url");
+
+        const fixed_url = addURLEndSlash(url);
+
+        const tech = await this.analyseWebPage(fixed_url);
+        return this.parseAnalysisResults(fixed_url, tech);
+    }
+}
+
+module.exports = Wappalyser;
