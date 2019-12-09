dc3a9c81295d10876de4495ad53b8f8300e43171
migudmigu <migud.carvalho@gmail.com>
2019-12-09 12:27:01 +0000

    refactor: Updated cve test suite with new format

--- test/CVEs/CVEs.js
+++ test/CVEs/CVEs.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const fc = require("fast-check");
-const { getCVEListPageUrl, parseRawCVE, scrapePage, parseLocalCacheCVEEntries, parseCVEsFile } = require("../../src/CVEs");
+const { getCVEListPageUrl, parseRawCVE, scrapePage } = require("../../src/CVEs");
+const { parseNoCacheCVEEntries, parseLocalCacheCVEEntries, parseCVEsFile } = require("../../src/CVEs");
 const { isValidCVE, isValidURL } = require("../../src/utils");
 const chai = require("chai"),
     expect = chai.expect,
@@ -58,11 +59,26 @@ describe("CVE list page scraping", () => {
         const reactnative_page_data = fs.readFileSync(reactnative_file_path, "utf-8");
         const reactnative_cve_list = await scrapePage(reactnative_page_data);
 
-        expect(reactnative_cve_list).to.deep.equal([
-            "2019-12164",
-            "2017-16028",
-            "2016-10697",
-        ]);
+        expect(reactnative_cve_list[0].id).to.equal("CVE-2019-12164");
+        // eslint-disable-next-line max-len
+        expect(reactnative_cve_list[0].description).to.equal("ubuntu-server.js in Status React Native Desktop before v0.57.8_mobile_ui allows Remote Code Execution.");
+        expect(reactnative_cve_list[0].exploits[0]).to.equal("https://www.exploit-db.com/search?cve=2019-12164");
+        expect(reactnative_cve_list[0].exploits[1]).to.equal("https://www.rapid7.com/db/?q=CVE-2019-12164&type=nexpose");
+        expect(reactnative_cve_list[0].exploits[2]).to.equal("https://search.circl.lu/?q=2019-12164");
+
+        expect(reactnative_cve_list[1].id).to.equal("CVE-2017-16028");
+        // eslint-disable-next-line max-len
+        expect(reactnative_cve_list[1].description).to.equal("react-native-meteor-oauth is a library for Oauth2 login to a Meteor server in React Native. The oauth Random Token is generated using a non-cryptographically strong RNG (Math.random()).");
+        expect(reactnative_cve_list[1].exploits[0]).to.equal("https://www.exploit-db.com/search?cve=2017-16028");
+        expect(reactnative_cve_list[1].exploits[1]).to.equal("https://www.rapid7.com/db/?q=CVE-2017-16028&type=nexpose");
+        expect(reactnative_cve_list[1].exploits[2]).to.equal("https://search.circl.lu/?q=2017-16028");
+
+        expect(reactnative_cve_list[2].id).to.equal("CVE-2016-10697");
+        // eslint-disable-next-line max-len
+        expect(reactnative_cve_list[2].description).to.equal("react-native-baidu-voice-synthesizer is a baidu voice speech synthesizer for react native. react-native-baidu-voice-synthesizer downloads resources over HTTP, which leaves it vulnerable to MITM attacks. It may be possible to cause remote code execution (RCE) by swapping out the requested resources with an attacker controlled copy if the attacker is on the network or positioned in between the user and the remote server.");
+        expect(reactnative_cve_list[2].exploits[0]).to.equal("https://www.exploit-db.com/search?cve=2016-10697");
+        expect(reactnative_cve_list[2].exploits[1]).to.equal("https://www.rapid7.com/db/?q=CVE-2016-10697&type=nexpose");
+        expect(reactnative_cve_list[2].exploits[2]).to.equal("https://search.circl.lu/?q=2016-10697");
 
         const apollojs_file_path = path.join(__dirname, "cve_list_page_apollojs.html");
         const apollojs_page_data = fs.readFileSync(apollojs_file_path, "utf-8");
@@ -73,6 +89,35 @@ describe("CVE list page scraping", () => {
 
     it("should fail when the page data is missing", () => {
         expect(scrapePage).to.throw("Missing page data");
+    });
+});
+
+describe("Fetched CVE entry parsing", () => {
+    it("should correctly parse fetched cve entries", () => {
+
+        // eslint-disable-next-line max-len
+        const [parsed_entry1, parsed_entry2, parsed_entry3] = parseNoCacheCVEEntries([["CVE-1111-1111", "Description 1"], ["CVE-2222-2222", "Description 2"], ["CVE-3333-3333", "Description 3"]]);
+
+        expect(parsed_entry1.id).to.equal("CVE-1111-1111");
+        expect(parsed_entry1.description).to.equal("Description 1");
+        // eslint-disable-next-line max-len
+        expect(parsed_entry1.exploits[0]).to.equal("https://www.exploit-db.com/search?cve=1111-1111");
+        expect(parsed_entry1.exploits[1]).to.equal("https://www.rapid7.com/db/?q=CVE-1111-1111&type=nexpose");
+        expect(parsed_entry1.exploits[2]).to.equal("https://search.circl.lu/?q=1111-1111");
+
+        expect(parsed_entry2.id).to.equal("CVE-2222-2222");
+        expect(parsed_entry2.description).to.equal("Description 2");
+        // eslint-disable-next-line max-len
+        expect(parsed_entry2.exploits[0]).to.equal("https://www.exploit-db.com/search?cve=2222-2222");
+        expect(parsed_entry2.exploits[1]).to.equal("https://www.rapid7.com/db/?q=CVE-2222-2222&type=nexpose");
+        expect(parsed_entry2.exploits[2]).to.equal("https://search.circl.lu/?q=2222-2222");
+
+        expect(parsed_entry3.id).to.equal("CVE-3333-3333");
+        expect(parsed_entry3.description).to.equal("Description 3");
+        // eslint-disable-next-line max-len
+        expect(parsed_entry3.exploits[0]).to.equal("https://www.exploit-db.com/search?cve=3333-3333");
+        expect(parsed_entry3.exploits[1]).to.equal("https://www.rapid7.com/db/?q=CVE-3333-3333&type=nexpose");
+        expect(parsed_entry3.exploits[2]).to.equal("https://search.circl.lu/?q=3333-3333");
     });
 });
 
