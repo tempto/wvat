90c81491e6dc45b01f0ba2af982c8a9a96475f77
Angelo Teixeira <imnotteixeira@gmail.com>
2019-12-16 17:08:32 +0000

    enhancement: added integration test for subdomain crawling

--- test/SubdomainCrawler.js
+++ test/SubdomainCrawler.js
@@ -1,6 +1,8 @@
 const { getSubdomainsList } = require("../src/SubdomainCrawler");
 const { getCompatibleWhitelistedSubdomains } = require("../src/commands/crawl");
 const fs = require("fs");
+const EventEmitter = require("events");
+const child_process = require("child_process");
 jest.mock("child_process");
 
 describe("Subdomain Crawler tests", () => {
@@ -23,5 +25,27 @@ describe("whitelist subdomains", () => {
     it("should only return subdomains of the specified domain", () => {
         fs.readFileSync.mockReturnValue("fe.up.pt\nni.fe.up.pt\nnotadomain\nnot.domain.pt");
         expect(new Set(getCompatibleWhitelistedSubdomains("doesnotmatter", "up.pt"))).toEqual(new Set(["ni.fe.up.pt", "fe.up.pt"]));
+    });
+});
+
+describe("Crawl Integration Test", () => {
+
+    it("should read the crawled subdomains", async () => {
+
+        const stdout_emitter = new EventEmitter();
+
+        const amass = new EventEmitter();
+        amass.stdout = stdout_emitter;
+
+        fs.existsSync.mockImplementation(() => true);
+        fs.readFileSync.mockImplementation(() => "domain1\ndomain2\n");
+        child_process.exec.mockImplementation(() => amass);
+
+        const subdomains = getSubdomainsList("test_domain.com", { timeout: 3 });
+        stdout_emitter.emit("data", "test-data");
+        amass.emit("close");
+
+        expect(await subdomains).toEqual(["domain1", "domain2"]);
+
     });
 });
